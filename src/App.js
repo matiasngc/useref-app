e66a3b189d3ba2685e8e45a5f6e6daa9cd60211d
Matías Guevara <matias.guevara99@hotmail.com>
2022-08-06 22:42:05 -0300

    Tutorial finalizado!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// import FocusApp from "./FocusApp";
+// import CopyApp from "./CopyApp";
+// import MediaPlayer from './MediaPlayer'
+// import ScrollAnimation from './ScrollAnimation'
+// import RenderApp from './RenderApp'
+import LoadingApp from './LoadingApp'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <FocusApp /> */}
+      {/* <CopyApp /> */}
+      {/* <MediaPlayer /> */}
+      {/* <ScrollAnimation /> */}
+      {/* <RenderApp /> */}
+      <LoadingApp />
     </div>
   );
 }
